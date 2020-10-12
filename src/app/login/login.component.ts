1b7686a5e48f3e99f0c4fc9f518eec87eee4c6b6
Hester Knight <hester2knight@mac.com>
2020-10-12 16:21:10 +0100

    basic login form tweak

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,8 +7,6 @@ import { User } from './user';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  // user property => username and password
-
   // Created an instance of user (object)
   user = new User('', '');
 

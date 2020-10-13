6dad6a21e1f38d401d93d12a80710be386643595
Hester Knight <hester2knight@mac.com>
2020-10-13 08:20:22 +0100

    tidyied

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './user';
 import { AuthService } from '../auth.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +12,14 @@ export class LoginComponent implements OnInit {
   // Created an instance of user (object)
   user = new User('', '');
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private location: Location) {}
 
   ngOnInit(): void {}
 
   onSubmit() {
     console.log('submit ');
-    this.authService.postUser(this.user).subscribe(res => console.log(res))
+    // TOKEN AUTH HERE: save token to local storage
+    this.authService.postUser(this.user).subscribe((res) => console.log(res));
+    this.router.navigate(['/resultslist']);
   }
 }

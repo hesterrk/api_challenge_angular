746a5924d835752f6749ae7c74a81c46b145b4f2
Hester Knight <hester2knight@mac.com>
2020-10-13 16:56:36 +0100

    eslint config

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -36,3 +36,5 @@ export class AuthService {
     return throwError(err.message || err);
   }
 }
+
+
--- src/app/result-list/result-list.component.ts
+++ src/app/result-list/result-list.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-result-list',
   templateUrl: './result-list.component.html',
-  styleUrls: ['./result-list.component.css']
+  styleUrls: ['./result-list.component.css'],
 })
 export class ResultListComponent implements OnInit {
+  defaultColDef = {
+    sortable: true,
+    filter: true,
+  };
+  columnDefs = [
+    { field: 'SiteName' },
+    { field: 'ProjectName' },
+    { field: 'DocumentName' },
+  ];
 
-  constructor() { }
+  rowData = [
+    { SiteName: 'site1', ProjectName: 'proj1', DocumentName: 'doc1' },
+    { SiteName: 'site2', ProjectName: 'proj2', DocumentName: 'doc2' },
+    { SiteName: 'site3', ProjectName: 'proj3', DocumentName: 'doc3' },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
